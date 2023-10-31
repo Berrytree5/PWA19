8fa4736e42775e0c596417048e779d39ff8f7948
saucyy <131738356+Berrytree5@users.noreply.github.com>
2023-10-31 12:22:30 -0700

    run build successful

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,81 +1,85 @@
+// Import necessary modules and plugins
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
-const { url } = require('inspector');
 
+// Create and export the Webpack configuration
 module.exports = () => {
   return {
+    // Set the build mode
     mode: 'development',
+
+    // Define the entry points for your application
     entry: {
       main: './src/js/index.js',
       install: './src/js/install.js'
     },
+
+    // Configure the output for your bundled files
     output: {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
+
+    // Configure and add plugins to your Webpack configuration
     plugins: [
-      // Add HtmlWebpackPlugin to generate HTML files
+      // HtmlWebpackPlugin generates HTML and injects bundles
       new HtmlWebpackPlugin({
-        template: './src/index.html',
-        filename: 'index.html',
-        chunks: ['main'],
-      }),
-      new HtmlWebpackPlugin({
-        template: './src/install.html',
-        filename: 'install.html',
-        chunks: ['install'],
+        template: './index.html', // Use this HTML template
+        title: 'Text Editor', // Set the title
       }),
 
-      // Add WebpackPwaManifest for manifest file
+      // InjectManifest adds a custom service worker
+      new InjectManifest({
+        swSrc: './src-sw.js', // Service worker source file
+        swDest: 'src-sw.js', // Destination for the service worker
+      }),
+
+      // WebpackPwaManifest generates a manifest file
       new WebpackPwaManifest({
-        name: 'Just Another Text Editor',
-        short_name: 'JATE',
-        description: 'Creates notes with or without active connection',
-        background_color: '#ffffff',
-        theme_color: '#2196F3',
+        fingerprints: false, // Disable fingerprints
+        inject: true, // Inject into HTML
+        name: 'Just another text editor', // App name
+        short_name: 'JATE', // Short name
+        description: 'Just another text editor!', // Description
+        background_color: '#225ca3', // Background color
+        theme_color: '#225ca3', // Theme color
+        start_url: '/', // Start URL
+        publicPath: '/', // Public path
         icons: [
           {
-            src: path.resolve('src/images/logo.png'),
-            sizes: [96, 128, 192, 256, 384, 512],
-            destination: path.join('assets', 'logo'),
+            src: path.resolve('src/images/logo.png'), // Icon source
+            sizes: [96, 128, 192, 256, 384, 512], // Icon sizes
+            destination: path.join('assets', 'icons'), // Destination path
           },
         ],
       }),
-
-      // Add InjectManifest for service worker
-      new InjectManifest({
-        swSrc: './src/sw.js',
-        swDest: 'src-sw.js',
-      }),
     ],
 
+    // Configure modules and loaders
     module: {
       rules: [
-        // Add CSS loader
+        // Load CSS files with style-loader and css-loader
         {
-          test: /\.css$/,
-          use: ['style-loader', 'css-loader'],
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"]
         },
-
-        // Add Babel loader
+        // Transpile JavaScript files with Babel
         {
-          test: /\.js$/,
+          test: /\.m?js$/,
           exclude: /node_modules/,
           use: {
-            loader: 'babel-loader',
+            loader: "babel-loader",
             options: {
-              presets: ['@babel/preset-env'],
-
-                plugins: [
-                          "@babel/plugin-proposal-object-rest-spread",
-
-                          "@babel/transorm-runtime",
-                ]
-            },
-          },
-        },
+              presets: ["@babel/preset-env"],
+              plugins: [
+                "@babel/plugin-proposal-object-rest-spread",
+                "@babel/transform-runtime"
+              ]
+            }
+          }
+        }
       ],
     },
   };
